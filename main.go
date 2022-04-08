f89082ef5c5976f1096d83ad7184af26cce89564
Aaron Alexander <aarona.arduino@gmail.com>
2022-04-07 22:46:54 -0500

    linear search

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+var arr = []int{-2, 3, 4, 7, 8, 9, 11, 13}
 
 func main() {
-    fmt.Println("Hello")
+	target := 11
+
+	result := search(arr, target)
+
+	fmt.Println(fmt.Sprintf("Searching for %d:", target))
+	if result < 0 {
+		fmt.Println("Could not find target in array.")
+	} else {
+		fmt.Printf("%d found at index %d\n", target, result)
+	}
+}
+
+func search(arr []int, target int) int {
+	defer elapsed("search")()
+	for i, num := range arr {
+		if num == target {
+			return i
+		}
+	}
+	return -1
+}
+
+func elapsed(what string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", what, time.Since(start))
+	}
 }
